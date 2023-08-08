697e13efee4c92973024e3b5edc4ad2603a38c8c
Samuel Desmond Abbey Darko <dehydrus223@gmail.com>
2023-08-08 14:05:57 +0000

    update

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 python3 -m pip install -r requirements.txt
-python3 manage.py makemigrations accounts --noinput
-python3 manage.py migrate accounts --noinput
 python3 manage.py makemigrations dashboard --database=second_db --noinput
 python3 manage.py migrate dashboard --database=second_db --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput --clear
